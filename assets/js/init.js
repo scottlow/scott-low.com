ae313ae8d052b5cd1a89aff9c42f158540d1abbe
Scott Low <slow@uvic.ca>
2017-04-09 23:18:14 -0700

    Added projects and speaking section

--- assets/js/init.js
+++ assets/js/init.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
     "use strict";
 
     /***************************************************************************/
-            /* NAVIGATION  */
+    /* NAVIGATION  */
     /***************************************************************************/
 
-      $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav();
 
     /**************************************************************************
                  SKILL BAR
     **************************************************************************/
 
-    $(".determinate").each(function(){
-      var width = $(this).text();
-      $(this).css("width", width)
-        .empty()
-        .append('<i class="fa fa-circle"></i>');
+    $(".determinate").each(function() {
+        var width = $(this).text();
+        $(this).css("width", width)
+            .empty()
+            .append('<i class="fa fa-circle"></i>');
     });
 
 
@@ -23,39 +23,44 @@ $(document).ready(function() {
              BLOG POST
     **************************************************************************/
 
-    jQuery(window).on('load', function(){ var $ = jQuery;
+    jQuery(window).on('load', function() {
+        var $ = jQuery;
         $('.blog').masonry({
-          itemSelector: '.blog-post',
-          columnWidth: '.blog-post',
-          percentPosition: true
+            itemSelector: '.blog-post',
+            columnWidth: '.blog-post',
+            percentPosition: true
         });
     });
 
 
     var height = $('.caption').height();
-        if($(window).width()){
-          $('#featured').css('height', height);
-          $('#featured img').css('height', height);
-        }
+    if ($(window).width()) {
+        $('#featured').css('height', height);
+        $('#featured img').css('height', height);
+    }
 
 
     /*************************************************************************
                 TOOLTIP
     **************************************************************************/
-    $('.tooltipped').tooltip({delay: 50});
+    $('.tooltipped').tooltip({
+        delay: 50
+    });
 
     /**************************************************************************
         WOW INIT
     **************************************************************************/
 
-    var wow = new WOW({ mobile: false });
+    var wow = new WOW({
+        mobile: false
+    });
     wow.init();
 
     /***************************************************************************
           CONTACT FORM
     ***************************************************************************/
 
-    $("#contactForm").validator().on("submit", function (event) {
+    $("#contactForm").validator().on("submit", function(event) {
         if (event.isDefaultPrevented()) {
             // handle the invalid form...
             formError();
@@ -66,8 +71,9 @@ $(document).ready(function() {
             submitForm();
         }
     });
-    function submitForm(){
-    // Initiate Variables With Form Content
+
+    function submitForm() {
+        // Initiate Variables With Form Content
         var name = $("#name").val();
         var email = $("#email").val();
         var message = $("#message").val();
@@ -76,28 +82,31 @@ $(document).ready(function() {
             type: "POST",
             url: "process.php",
             data: "name=" + name + "&email=" + email + "&message=" + message,
-            success : function(text){
-                if (text == "success"){
+            success: function(text) {
+                if (text == "success") {
                     formSuccess();
                 } else {
                     formError();
-                    submitMSG(false,text);
+                    submitMSG(false, text);
                 }
             }
         });
     }
-    function formSuccess(){
+
+    function formSuccess() {
         $("#contactForm")[0].reset();
         submitMSG(true, "Message Sent!")
     }
-    function formError(){
+
+    function formError() {
         $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
-        function(){
-            $(this).removeClass();
-        });
+            function() {
+                $(this).removeClass();
+            });
     }
-    function submitMSG(valid, msg){
-        if(valid){
+
+    function submitMSG(valid, msg) {
+        if (valid) {
             var msgClasses = "h3 text-center fadeInUp animated text-success";
         } else {
             var msgClasses = "h3 text-center text-danger";
@@ -113,87 +122,214 @@ $(document).ready(function() {
 
     $('.sa-view-project-detail').on('click', function(event) {
         event.preventDefault();
-        var href          = $(this).attr('href') + ' ' + $(this).attr('data-action'),
-            dataShow      = $('#project-gallery-view'),
-            dataShowMeta  = $('#project-gallery-view meta'),
-            dataHide      = $('#portfolio-item'),
-            preLoader     = $('#loader'),
-            backBtn       = $('#back-button'),
-            filterBtn     = $('#filter-button');
+        var href = $(this).attr('href') + ' ' + $(this).attr('data-action'),
+            dataShow = $('#project-gallery-view'),
+            dataShowMeta = $('#project-gallery-view meta'),
+            dataHide = $('#portfolio-item'),
+            preLoader = $('#loader'),
+            backBtn = $('#back-button'),
+            filterBtn = $('#filter-button');
 
-        dataHide.animate( { 'marginLeft':'-120%' }, { duration: 400, queue: false } );
-        filterBtn.animate( { 'marginLeft':'-120%' }, { duration: 400, queue: false } );
+        dataHide.animate({
+            'marginLeft': '-120%'
+        }, {
+            duration: 400,
+            queue: false
+        });
+        filterBtn.animate({
+            'marginLeft': '-120%'
+        }, {
+            duration: 400,
+            queue: false
+        });
         dataHide.fadeOut(400);
         filterBtn.fadeOut(400);
-        setTimeout( function() { preLoader.show(); }, 400);
-        setTimeout( function() {
-            dataShow.load( href, function() {
+        setTimeout(function() {
+            preLoader.show();
+        }, 400);
+        setTimeout(function() {
+            dataShow.load(href, function() {
                 dataShowMeta.remove();
                 preLoader.hide();
                 dataShow.fadeIn(600);
                 backBtn.fadeIn(600);
             });
-        },800);
+        }, 800);
     });
 
     $('#back-button').on('click', function(event) {
         event.preventDefault();
-        var dataShow    = $('#portfolio-item'),
-            dataHide    = $('#project-gallery-view'),
-            filterBtn   = $('#filter-button');
+        var dataShow = $('#portfolio-item'),
+            dataHide = $('#project-gallery-view'),
+            filterBtn = $('#filter-button');
 
-        $("[data-animate]").each( function() {
+        $("[data-animate]").each(function() {
             $(this).addClass($(this).attr('data-animate'));
         });
 
         dataHide.fadeOut(400);
         $(this).fadeOut(400);
-        setTimeout(function(){
-            dataShow.animate( { 'marginLeft': '0' }, { duration: 400, queue: false } );
-            filterBtn.animate( { 'marginLeft': '0' }, { duration: 400, queue: false } );
+        setTimeout(function() {
+            dataShow.animate({
+                'marginLeft': '0'
+            }, {
+                duration: 400,
+                queue: false
+            });
+            filterBtn.animate({
+                'marginLeft': '0'
+            }, {
+                duration: 400,
+                queue: false
+            });
             dataShow.fadeIn(400);
             filterBtn.fadeIn(400);
-        },400);
-        setTimeout(function(){
+        }, 400);
+        setTimeout(function() {
             dataShow.find('.fadeInRight, .fadeInLeft, .fadeInUp, .fadeInDown').removeClass('fadeInRight').removeClass('fadeInLeft').removeClass('fadeInUp').removeClass('fadeInDown');
-        },1500);
+        }, 1500);
     });
 
 });
 
-    /***************************************************************************
-                MAP
-    ***************************************************************************/
+/***************************************************************************
+            MAP
+***************************************************************************/
 
-    google.maps.event.addDomListener(window, 'load', init);
-    function init() {
-        var lat = 47.613651;
-        var long = -122.3205138;
-        var mapOptions = {
-          zoom: 17,
-          scrollwheel: false,
-          navigationControl: false,
-          center: new google.maps.LatLng(lat,long),
-          styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},
-          {"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},
-          {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},
-          {"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},
-          {"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},
-          {"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},
-          {"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},
-          {"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},
-          {"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},
-          {"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},
-          {"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},
-          {"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},
-          {"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
-      };
-      var mapElement = document.getElementById('map');
-      var map = new google.maps.Map(mapElement, mapOptions);
-      var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(lat, long),
-          map: map,
-          title: '1427 11th Avenue'
-      });
-    }
+google.maps.event.addDomListener(window, 'load', init);
 
+function init() {
+    var lat = 47.6393225;
+    var long = -122.1283833;
+    var mapOptions = {
+        zoom: 14,
+        scrollwheel: false,
+        navigationControl: false,
+        mapTypeControl: false,
+        streetViewControl: false,
+        center: new google.maps.LatLng(lat, long),
+        styles: [{
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#e9e9e9"
+            }, {
+                "lightness": 17
+            }]
+        }, {
+            "featureType": "landscape",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#f5f5f5"
+            }, {
+                "lightness": 20
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "color": "#ffffff"
+            }, {
+                "lightness": 17
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "color": "#ffffff"
+            }, {
+                "lightness": 29
+            }, {
+                "weight": 0.2
+            }]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#ffffff"
+            }, {
+                "lightness": 18
+            }]
+        }, {
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#ffffff"
+            }, {
+                "lightness": 16
+            }]
+        }, {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#f5f5f5"
+            }, {
+                "lightness": 21
+            }]
+        }, {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#dedede"
+            }, {
+                "lightness": 21
+            }]
+        }, {
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+                "visibility": "on"
+            }, {
+                "color": "#ffffff"
+            }, {
+                "lightness": 16
+            }]
+        }, {
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "saturation": 36
+            }, {
+                "color": "#333333"
+            }, {
+                "lightness": 40
+            }]
+        }, {
+            "elementType": "labels.icon",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "transit",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#f2f2f2"
+            }, {
+                "lightness": 19
+            }]
+        }, {
+            "featureType": "administrative",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "color": "#fefefe"
+            }, {
+                "lightness": 20
+            }]
+        }, {
+            "featureType": "administrative",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "color": "#fefefe"
+            }, {
+                "lightness": 17
+            }, {
+                "weight": 1.2
+            }]
+        }]
+    };
+    var mapElement = document.getElementById('map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, long),
+        map: map,
+        title: '1427 11th Avenue'
+    });
+}
