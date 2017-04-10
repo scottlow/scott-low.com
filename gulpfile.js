f8a93a6707242b472ed1a2a252986683bbc79f24
Scott Low <slow@uvic.ca>
2017-04-10 01:15:58 -0700

    Refactor folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,25 @@ var less = require('gulp-less');
 var path = require('path');
 
 gulp.task('webserver', function() {
-  gulp.src('.')
+  gulp.src('./site')
     .pipe(webserver({
       livereload: true,
-      directoryListing: true,
-      open: true
+      directoryListing: false,
+      open: true,
+      defaultFile: 'index.html'
     }));
 });
 
 gulp.task('less', function () {
-  return gulp.src('./assets/less/**/*.less')
+  return gulp.src('./site/assets/less/**/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest('./site/assets/css'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./assets/less/**/*.less', ['less'])
+    gulp.watch('./site/assets/less/**/*.less', ['less'])
 });
 
 gulp.task('default', ['webserver', 'less', 'watch']);
